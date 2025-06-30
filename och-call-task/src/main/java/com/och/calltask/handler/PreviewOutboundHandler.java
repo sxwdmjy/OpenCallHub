d3973aa6279fe753a4f00007d00814ad850ecbff
danmo <775144922@qq.com>
2025-06-30 18:36:44 +0800

    customer code commit

--- och-call-task/src/main/java/com/och/calltask/handler/PreviewOutboundHandler.java
+++ och-call-task/src/main/java/com/och/calltask/handler/PreviewOutboundHandler.java
@@ -1,24 +1,16 @@
 package com.och.calltask.handler;
 
-import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONArray;
 import com.och.calltask.domain.CallTaskRoundsConf;
 import com.och.calltask.domain.entity.CallTask;
-import com.och.calltask.domain.query.DataSourceContactQuery;
-import com.och.calltask.domain.vo.DataSourcesContactVo;
 import com.och.calltask.service.ICallTaskService;
-import com.och.calltask.service.IOchDataSourcesService;
 import com.och.common.config.redis.RedisService;
-import com.och.common.constant.CacheConstants;
-import com.och.common.enums.SipAgentStatusEnum;
-import com.och.system.domain.vo.agent.SipAgentStatusVo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 /**
@@ -33,7 +25,6 @@ import java.util.Objects;
 public class PreviewOutboundHandler implements CallTaskHandler {
 
     private final ICallTaskService callTaskService;
-    private final IOchDataSourcesService dataSourcesService;
     private final RedisService redisService;
 
     @Override
@@ -51,26 +42,16 @@ public class PreviewOutboundHandler implements CallTaskHandler {
 
 
         List<CallTaskRoundsConf> callTaskRoundsConfs = JSONArray.parseArray(callTask.getRoundsConf(), CallTaskRoundsConf.class);
-        if(CollectionUtils.isEmpty(callTaskRoundsConfs)){
+        if (CollectionUtils.isEmpty(callTaskRoundsConfs)) {
             log.info("任务轮次配置为空,任务ID：{}, 数据源ID:{}", taskId, sourceId);
             return;
         }
         Integer currentRound = callTask.getCurrentRound();
         CallTaskRoundsConf callTaskRoundsConf = callTaskRoundsConfs.stream().filter(conf -> conf.getRounds().equals(currentRound)).findFirst().orElse(null);
-        if(Objects.isNull(callTaskRoundsConf)){
-            log.info("任务轮次配置不存在,任务ID：{}, 数据源ID:{}, currentRound:{}", taskId, sourceId,currentRound);
+        if (Objects.isNull(callTaskRoundsConf)) {
+            log.info("任务轮次配置不存在,任务ID：{}, 数据源ID:{}, currentRound:{}", taskId, sourceId, currentRound);
             return;
         }
-
-
-        DataSourceContactQuery query = new DataSourceContactQuery();
-        query.setSourceId(sourceId);
-        List<DataSourcesContactVo> contactList = dataSourcesService.getContactPageList(query);
-        if (CollectionUtils.isEmpty(contactList)) {
-            log.info("数据源联系人为空,任务ID：{}, 数据源ID:{}", taskId, sourceId);
-            return;
-        }
-
 
 
     }
