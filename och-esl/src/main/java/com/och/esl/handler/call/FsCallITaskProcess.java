d797a2f934e5fe1d4ad55b07f7845b635630a99d
danmo <775144922@qq.com>
2025-09-29 18:57:56 +0800

    预测外呼

--- och-esl/src/main/java/com/och/esl/handler/call/FsCallITaskProcess.java
+++ och-esl/src/main/java/com/och/esl/handler/call/FsCallITaskProcess.java
@@ -1,14 +1,11 @@
 package com.och.esl.handler.call;
 
-import cn.hutool.core.date.DateUtil;
 import com.och.common.annotation.EslProcessName;
 import com.och.common.domain.CallInfo;
 import com.och.common.domain.ChannelInfo;
 import com.och.common.enums.ProcessEnum;
 import com.och.common.enums.RouteTypeEnum;
 import com.och.esl.handler.route.FsAbstractRouteHandler;
-import com.och.system.domain.entity.CallSchedule;
-import com.och.system.domain.vo.route.CallRouteVo;
 import lombok.extern.slf4j.Slf4j;
 import org.freeswitch.esl.client.transport.event.EslEvent;
 import org.springframework.stereotype.Component;
@@ -30,11 +27,11 @@ public class FsCallITaskProcess extends FsAbstractCallProcess {
     public void handler(String address, EslEvent event, CallInfo callInfo, ChannelInfo lfsChannelInfo) {
         log.info("进入callTask电话: callId:{} caller:{} called:{} uniqueId:{}, otherUniqueId:{}", callInfo.getCallId(), callInfo.getCaller(), callInfo.getCallee(), lfsChannelInfo.getOtherUniqueId(), lfsChannelInfo.getUniqueId());
         lfsCallCacheService.saveCallInfo(callInfo);
-        lfsCallCacheService.saveCallRel(lfsChannelInfo.getUniqueId(),callInfo.getCallId());
+        lfsCallCacheService.saveCallRel(lfsChannelInfo.getUniqueId(), callInfo.getCallId());
         Long agentId = callInfo.getAgentId();
         FsAbstractRouteHandler routeHandler = routeFactory.factory(RouteTypeEnum.AGENT.getType());
-        if(Objects.nonNull(routeHandler)){
-            routeHandler.handler(address, callInfo,lfsChannelInfo.getUniqueId(), String.valueOf(agentId));
+        if (Objects.nonNull(routeHandler)) {
+            routeHandler.handler(address, callInfo, lfsChannelInfo.getUniqueId(), String.valueOf(agentId));
         }
     }
 
