5e45ac63a0a79762f2c57a40e9f893cb3a54ce6f
danmo <775144922@qq.com>
2025-10-21 15:05:57 +0800

    fix

--- och-common/src/main/java/com/och/common/exception/NodeExecutionException.java
+++
@@ -1,7 +0,0 @@
-package com.och.common.exception;
-
-public class NodeExecutionException extends RuntimeException{
-    public NodeExecutionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
--- och-common/src/main/java/com/och/common/exception/UnsupportedNodeTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.och.common.exception;
-
-public class UnsupportedNodeTypeException extends RuntimeException {
-    public UnsupportedNodeTypeException(String message) {
-        super(message);
-    }
-}
